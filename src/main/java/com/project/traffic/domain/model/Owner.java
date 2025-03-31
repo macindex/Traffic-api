2f66779bc23c4bdc6e72b500b086959be5364ac7
macindex <machollanda@gmail.com>
2025-03-30 23:22:06 -0300

    lombok

--- src/main/java/com/project/traffic/domain/model/Owner.java
+++ src/main/java/com/project/traffic/domain/model/Owner.java
@@ -1,5 +1,10 @@
 package com.project.traffic.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Owner {
 
     private Long id;
@@ -7,35 +12,4 @@ public class Owner {
     private String email;
     private String telefone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
