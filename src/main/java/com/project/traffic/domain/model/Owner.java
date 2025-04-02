f63799700a4e6ea17f4a2741f4f73f20cf2c373e
macindex <machollanda@gmail.com>
2025-04-01 22:54:00 -0300

    owner

--- src/main/java/com/project/traffic/domain/model/Owner.java
+++ src/main/java/com/project/traffic/domain/model/Owner.java
@@ -1,15 +1,24 @@
 package com.project.traffic.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+//@Table(name = "tb_owner")
 public class Owner {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+    @Column(name = "celphone")
     private String telefone;
 
 }
