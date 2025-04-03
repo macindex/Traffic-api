d29f2ffe5dbb760d7e4b028c69089cb01058b0e3
macindex <machollanda@gmail.com>
2025-04-03 20:41:30 -0300

    repository

---
+++ src/main/java/com/project/traffic/domain/repository/OwnerRepository.java
@@ -0,0 +1,10 @@
+package com.project.traffic.domain.repository;
+
+import com.project.traffic.domain.model.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+}
