178daa2c78addbcd7fe4f2f8f52a39847ecfc12d
macindex <machollanda@gmail.com>
2025-04-21 12:42:21 -0300

    method reference

--- src/main/java/com/project/traffic/api/controller/OwnerController.java
+++ src/main/java/com/project/traffic/api/controller/OwnerController.java
@@ -6,14 +6,19 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/owners")
 public class OwnerController {
 
     private final OwnerRepository ownerRepository;
@@ -22,10 +27,29 @@ public class OwnerController {
 //        this.ownerRepository = ownerRepository;
 //    }
 
-    @GetMapping("/owners")
-    public List<Owner> listar(){
+    @GetMapping
+    public List<Owner> listar() {
 //        return ownerRepository.findByNome("Carlos andrade");
-        return ownerRepository.findByNameContaining("and");
+        return ownerRepository.findAll();
+    }
+    @GetMapping("/{ownerId}")
+    public ResponseEntity<Owner> search(@PathVariable Long ownerId){
+        return ownerRepository.findbyId(ownerId)
+                .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+//    @GetMapping("/owners/{ownerId}")
+//    public Owner search(@PathVariable Long ownerId){
+//        Optional<Owner> owner = ownerRepository.findById(ownerId);
+//        return owner.orElse(null);
+//    }
+//    @GetMapping("/owners/{ownerId}")
+//    public ResponseEntity search(@PathVariable Long ownerId){
+//        Optional<Owner> owner = ownerRepository.findById(ownerId);
+//        if (owner.isPresent()){
+//            return ResponseEntity.ok(owner.get());
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
 
 
 //        var owner1 = new Owner();
@@ -40,5 +64,5 @@ public class OwnerController {
 //        owner2.setEmail("pablodoarrocha@hotmail.com");
 //        owner2.setTelefone("34 988887-9999");
 //        return Arrays.asList(owner1, owner2);
-    }
+
 }
--- src/main/java/com/project/traffic/domain/repository/OwnerRepository.java
+++ src/main/java/com/project/traffic/domain/repository/OwnerRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
-    List<Owner> findByNameContaining(String name);
+//    List<Owner> findByNameContaining(String name);
+//    List<Owner> findById(Long OwnerId)
+
+    Optional<Owner> findbyId(Long ownerId);
 }
