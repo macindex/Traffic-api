b0900e523ce2240d6943341602936cb83533a3c4
macindex <machollanda@gmail.com>
2025-05-10 18:07:34 -0300

    RFC 7807

--- src/main/java/com/project/traffic/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/project/traffic/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,9 +4,10 @@ import com.project.traffic.domain.exception.BusinessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<String> capt(BusinessException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
